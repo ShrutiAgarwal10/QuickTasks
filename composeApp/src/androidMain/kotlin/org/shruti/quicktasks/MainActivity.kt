9ec837a16a759a02ce9f58ac19fe9f4e438c1996
Shruti <s4shruti10@gmail.com>
2025-04-14 21:40:38 +0530

    pass DB

--- composeApp/src/androidMain/kotlin/org/shruti/quicktasks/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/shruti/quicktasks/MainActivity.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import org.shruti.quicktasks.database.AndroidDatabaseDriverFactory
+import org.shruti.quicktasks.database.LocalDatabase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            App(
+                LocalDatabase(
+                    databaseDriverFactory = AndroidDatabaseDriverFactory(applicationContext)
+                )
+            )
         }
     }
 }
@@ -19,5 +26,7 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+    App(LocalDatabase(
+        databaseDriverFactory = AndroidDatabaseDriverFactory(LocalContext.current)
+    ))
 }
--- composeApp/src/iosMain/kotlin/org/shruti/quicktasks/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/shruti/quicktasks/MainViewController.kt
@@ -1,5 +1,11 @@
 package org.shruti.quicktasks
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.shruti.quicktasks.database.IOSDatabaseDriverFactory
+import org.shruti.quicktasks.database.LocalDatabase
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { App(
+    LocalDatabase(
+        databaseDriverFactory = IOSDatabaseDriverFactory()
+    )
+) }
